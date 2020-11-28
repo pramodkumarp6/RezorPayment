3d1b00a15ba5bb57d85f31f8284a055599c242e9
pramod <pramodkumarp6@gmail.com>
2020-11-28 12:26:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rezorpayment/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.rezorpayment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.razorpay.Checkout;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private Button startpayment;
+    private EditText orderamount;
+    private String TAG =" main";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startpayment = (Button) findViewById(R.id.startpayment);
+        orderamount = (EditText) findViewById(R.id.orderamount);
+
+        startpayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(orderamount.getText().toString().equals(""))
+                {
+                    Toast.makeText(MainActivity.this, "Amount is empty", Toast.LENGTH_LONG).show();
+                }else {
+                    startPayment();
+                }
+            }
+        });
+    }
+
+    public void startPayment() {
+        /**
+         * You need to pass current activity in order to let Razorpay create CheckoutActivity
+         */
+        final Activity activity = this;
+        final Checkout co = new Checkout();
+        try {
+            JSONObject options = new JSONObject();
+            options.put("name", "Gravitation pvt ltd");
+            options.put("description", "App Payment");
+            //You can omit the image option to fetch the image from dashboard
+            options.put("image", "https://rzp-mobile.s3.amazonaws.com/images/rzp.png");
+            options.put("currency", "INR");
+            String payment = orderamount.getText().toString();
+            // amount is in paise so please multiple it by 100
+            //Payment failed Invalid amount (should be passed in integer paise. Minimum value is 100 paise, i.e. ₹ 1)
+            double total = Double.parseDouble(payment);
+            total = total * 100;
+            options.put("amount", total);
+            JSONObject preFill = new JSONObject();
+            preFill.put("email", "pramodkumarp6@gmail.com");
+            preFill.put("contact", "9717194480");
+            options.put("prefill", preFill);
+            co.open(activity, options);
+        } catch (Exception e) {
+            Toast.makeText(activity, "Error in payment: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    public void onPaymentSuccess(String s) {
+        // payment successfull pay_DGU19rDsInjcF2
+        Log.e(TAG, " payment successfull "+ s.toString());
+        Toast.makeText(this, "Payment successfully done! " +s, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onPaymentError(int i, String s) {
+        Log.e(TAG,  "error code "+String.valueOf(i)+" -- Payment failed "+s.toString()  );
+        try {
+            Toast.makeText(this, "Payment error please try again", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.e("OnPaymentError", "Exception in onPaymentError", e);
+        }
+    }
+}
